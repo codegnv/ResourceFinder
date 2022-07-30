9883d7016908ee6bbe9f3f7b8df4805766134b6b
Clark <clark.wood@protonmail.com>
2022-07-30 09:35:44 -0400

    added ResultsHeader component, which contains HrLine. Probably still need to fix spacing, etc

---
+++ src/components/Results/Header/Header.tsx
@@ -0,0 +1,18 @@
+import { MedHrLine } from '../../HrLine';
+
+export interface IResultsHeaderProps {
+  begin: number,
+  end: number,
+  total: number,
+}
+
+function ResultsHeader({ begin, end, total }: IResultsHeaderProps) {
+  return (
+    <>
+      <p>Showing <span style={{fontWeight: 'bold'}}>{begin} - {end}</span> of <span style={{fontWeight: 'bold'}}>{total.toLocaleString()}</span> results</p>
+      <MedHrLine />
+    </>
+  )
+}
+
+export default ResultsHeader
