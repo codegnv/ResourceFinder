aa82c9977f76e8774d0d9c8c5160a40348400866
Eric Hosford <HosfordDotMe@users.noreply.github.com>
2022-08-14 21:32:09 -0400

    Upgrade dependencies RF-57 (#21)

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextTranslate = require('next-translate')
-const nextConfig = {
-  reactStrictMode: true,
-}
 
 module.exports = nextTranslate({
-  nextConfig,
+  reactStrictMode: true,
 })
