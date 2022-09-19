2a0728f275a9cc3027cb25486a19729ae3e74240
CowboyAragorn <jacobman0313@gmail.com>
2022-09-19 11:42:26 -0400

    feat: add clear all results button to results indicator

--- src/components/Results/ResultsHeader/ResultsHeader.tsx
+++ src/components/Results/ResultsHeader/ResultsHeader.tsx
@@ -1,5 +1,8 @@
 import { MedHrLine } from '../../shared/HrLine'
-
+import { useDispatch } from 'react-redux'
+import { clearDepartmentsSelection } from 'src/components/Tabs/Content/Departments/departmentsSlice'
+import { clearTagSelection } from 'src/components/Tabs/Content/Categories/categoriesSlice'
+import { closeSearchbar, updateSearchText } from 'src/components/Header/Search/searchSlice'
 export interface IResultsHeaderProps {
   begin: number
   end: number
@@ -7,6 +10,13 @@ export interface IResultsHeaderProps {
 }
 
 export function ResultsHeader({ begin, end, total }: IResultsHeaderProps) {
+  const dispatch = useDispatch()
+  const handleClearAllResults = () => {
+    dispatch(clearDepartmentsSelection())
+    dispatch(clearTagSelection())
+    dispatch(closeSearchbar())
+    dispatch(updateSearchText(''))
+  }
   return (
     <>
       Showing{' '}
@@ -14,6 +24,7 @@ export function ResultsHeader({ begin, end, total }: IResultsHeaderProps) {
         {begin} - {end}
       </span>{' '}
       of <span style={{ fontWeight: 'bold' }}>{total.toLocaleString()}</span> results
+      <button onClick={handleClearAllResults}> Clear All Results</button>
       <MedHrLine />
     </>
   )
